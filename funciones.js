1aced96b9e8e102ceda9da699763a9a4878f697d
Ceciliafunes <144635090+Ceciliafunes@users.noreply.github.com>
2023-09-11 22:43:58 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,20 @@
+document.getElementById("boton_ingles").addEventListener("click", function () {
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML = "Poseo un nivel de ingles inermedio" 
+ });  
+
+
+ document.getElementById("boton_italiano").addEventListener("click", function () {
+    document.getElementById("italiano").innerHTML = "Poseo un nivel de italiano basico" 
+ });  
+
+
+ document.getElementById("boton_contable").addEventListener("click", function () {
+    document.getElementById("contable").innerHTML = "Poseo amplios conocimientos contables e impositivos." 
+
+ });  
+
+ document.getElementById("boton_electronica").addEventListener("click", function () {
+    document.getElementById("electro").innerHTML = "Poseo un nivel de conocimientos en electronica intermedio.";
+ });  
+
